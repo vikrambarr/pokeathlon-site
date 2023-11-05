35aadd8dd9e6ab858ddf2ede71b4ea961636b414
vikrambarr <37145312+vikrambarr@users.noreply.github.com>
2023-11-05 12:06:50 +0000

    hopefully even more mobile friendly

--- script.js
+++ script.js
@@ -1,56 +1,61 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const container = document.getElementById('container');
 
-    container.addEventListener('click', function(e) {
-        e.preventDefault();
-    });
+    if(!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        const marqueeText = document.getElementsByClassName('poke-marquee');
+        for (let marqueeElem of marqueeText) marqueeElem.style.display = 'block';
 
-    container.addEventListener('dblclick', function(e) {
-        const iconWrapper = e.target.closest('.icon-wrapper');
-        if (iconWrapper) {
-            const link = iconWrapper.querySelector('a');
-            if (link) window.open(link.href, '_blank');
-        }
-    });
+        const container = document.getElementById('container');
 
-    let draggedItem = null;
-    let initialMouseX = 0;
-    let initialMouseY = 0;
-    let initialIconX = 0;
-    let initialIconY = 0;
-
-    container.addEventListener('mousedown', function(e) {
-        const selected = document.querySelector('.selected');
-        if (selected) selected.classList.remove('selected');
-
-        if (e.target.closest('.icon-wrapper')) {
-            e.target.closest('.icon-wrapper').classList.add('selected');
+        container.addEventListener('click', function(e) {
             e.preventDefault();
-        }
+        });
 
-        e.preventDefault();
-        draggedItem = e.target.closest('.icon-wrapper');
-        if (draggedItem) {
-            initialMouseX = e.clientX;
-            initialMouseY = e.clientY;
-            initialIconX = (draggedItem.offsetLeft / container.offsetWidth) * 100;
-            initialIconY = (draggedItem.offsetTop / container.offsetHeight) * 100;
+        container.addEventListener('dblclick', function(e) {
+            const iconWrapper = e.target.closest('.icon-wrapper');
+            if (iconWrapper) {
+                const link = iconWrapper.querySelector('a');
+                if (link) window.open(link.href, '_blank');
+            }
+        });
 
-            const onMouseMove = (e) => {
-                const deltaXPercent = ((e.clientX - initialMouseX) / container.offsetWidth) * 100;
-                const deltaYPercent = ((e.clientY - initialMouseY) / container.offsetHeight) * 100;
-                draggedItem.style.left = `${initialIconX + deltaXPercent}%`;
-                draggedItem.style.top = `${initialIconY + deltaYPercent}%`;
-            };
+        let draggedItem = null;
+        let initialMouseX = 0;
+        let initialMouseY = 0;
+        let initialIconX = 0;
+        let initialIconY = 0;
 
-    
-            const onMouseUp = () => {
-                document.removeEventListener('mousemove', onMouseMove);
-                document.removeEventListener('mouseup', onMouseUp);
-            };
-    
-            document.addEventListener('mousemove', onMouseMove);
-            document.addEventListener('mouseup', onMouseUp);
-        }
-    });
+        container.addEventListener('mousedown', function(e) {
+            const selected = document.querySelector('.selected');
+            if (selected) selected.classList.remove('selected');
+
+            if (e.target.closest('.icon-wrapper')) {
+                e.target.closest('.icon-wrapper').classList.add('selected');
+                e.preventDefault();
+            }
+
+            e.preventDefault();
+            draggedItem = e.target.closest('.icon-wrapper');
+            if (draggedItem) {
+                initialMouseX = e.clientX;
+                initialMouseY = e.clientY;
+                initialIconX = (draggedItem.offsetLeft / container.offsetWidth) * 100;
+                initialIconY = (draggedItem.offsetTop / container.offsetHeight) * 100;
+
+                const onMouseMove = (e) => {
+                    const deltaXPercent = ((e.clientX - initialMouseX) / container.offsetWidth) * 100;
+                    const deltaYPercent = ((e.clientY - initialMouseY) / container.offsetHeight) * 100;
+                    draggedItem.style.left = `${initialIconX + deltaXPercent}%`;
+                    draggedItem.style.top = `${initialIconY + deltaYPercent}%`;
+                };
+        
+                const onMouseUp = () => {
+                    document.removeEventListener('mousemove', onMouseMove);
+                    document.removeEventListener('mouseup', onMouseUp);
+                };
+        
+                document.addEventListener('mousemove', onMouseMove);
+                document.addEventListener('mouseup', onMouseUp);
+            }
+        });
+    }
 });
